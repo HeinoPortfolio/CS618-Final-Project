e60f2c5f121560bbf9f78e2f8d53aa4f5efcb3d6
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-10-03 12:19:26 +0000

    chore:  started setting up the backend

---
+++ backend/src/db/models/recipe.js
@@ -0,0 +1,14 @@
+// Model for the recipe for the final project
+import mongoose, { Schema } from 'mongoose'
+
+const recipeSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: String,
+    ingredientList: String,
+    imageURL: String,
+  },
+  { timestamps: true },
+)
+
+export const Recipe = mongoose.model('recipe', recipeSchema)
