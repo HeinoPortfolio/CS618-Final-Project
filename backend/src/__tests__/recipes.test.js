db4c48016c63c4aa221c59c7a1f0314779d5e921
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-10-03 20:10:06 +0000

    chore: created list recipe service and created tests

--- backend/src/__tests__/recipes.test.js
+++ backend/src/__tests__/recipes.test.js
@@ -1,7 +1,49 @@
 import mongoose from 'mongoose'
-import { describe, expect, test } from '@jest/globals'
-import { createRecipe } from '../services/recipes.js'
+import { describe, expect, test, beforeEach } from '@jest/globals'
+import {
+  createRecipe,
+  listAllRecipes,
+  listRecipesByAuthor,
+} from '../services/recipes.js'
 import { Recipe } from '../db/models/recipe.js'
+
+const sampleRecipes = [
+  {
+    title: 'Sample Recipe 1',
+    author: 'Joe Doe',
+    ingredientList: 'Some ingredients go here!!!!',
+    imageURL: 'http://someUrl1.com',
+  },
+  {
+    title: 'Sample Recipe 2',
+    author: 'Jay Smith',
+    ingredientList: 'Some ingredients go here again!!!!',
+    imageURL: 'http://someUrl2.com',
+  },
+  {
+    title: 'Sample Recipe 3',
+    author: 'John Brown',
+    ingredientList: 'Some ingredients go here again with ingredients!!!!',
+    imageURL: 'http://someUrl3.com',
+  },
+  { title: 'Sample Recipe 4 only title and nothing else' },
+]
+
+// Before each  ===================================================================
+// To be executed to create items for the database =================================
+let createdSampleRecipes = []
+
+beforeEach(async () => {
+  await Recipe.deleteMany({})
+
+  createdSampleRecipes = []
+
+  for (const recipe of sampleRecipes) {
+    const createdRecipe = new Recipe(recipe)
+
+    createdSampleRecipes.push(await createdRecipe.save())
+  }
+})
 
 // Tests for Recipe application ===============================================
 // =============================================================================
@@ -47,5 +89,38 @@ describe('Creating recipes', () => {
 
     const createdRecipe = await createRecipe(recipe)
     expect(createdRecipe._id).toBeInstanceOf(mongoose.Types.ObjectId)
+  })
+})
+
+// Tests for listing recipes ==================================================
+describe('Listing recipes', () => {
+  test('Should return all recipes', async () => {
+    const recipes = await listAllRecipes()
+    expect(recipes.length).toEqual(createdSampleRecipes.length)
+  })
+  test('Should return all recipes sorted by creation date in descending order', async () => {
+    const recipes = await listAllRecipes()
+    const sortedSampleRecipes = createdSampleRecipes.sort(
+      (a, b) => b.createdAt - a.createdAt,
+    )
+    expect(recipes.map((recipe) => recipe.createdAt)).toEqual(
+      sortedSampleRecipes.map((recipe) => recipe.createdAt),
+    )
+  })
+  test('Should take into account the sorting options', async () => {
+    const recipes = await listAllRecipes({
+      sortBy: 'updatedAt',
+      sortOrder: 'ascending',
+    })
+    const sortedSampleRecipes = createdSampleRecipes.sort(
+      (a, b) => a.updatedAt - b.updatedAt,
+    )
+    expect(recipes.map((recipe) => recipe.updatedAt)).toEqual(
+      sortedSampleRecipes.map((recipe) => recipe.updatedAt),
+    )
+  })
+  test('Should be able to filter recipes by the author', async () => {
+    const recipes = await listRecipesByAuthor('Joe Doe')
+    expect(recipes.length).toBe(1)
   })
 })
--- backend/src/services/recipes.js
+++ backend/src/services/recipes.js
@@ -10,3 +10,21 @@ export async function createRecipe({
 
   return await recipe.save()
 }
+// List all the recipes =======================================================
+// It is a helper/common functionality ========================================
+async function listRecipes(
+  query = {},
+  { sortBy = 'createdAt', sortOrder = 'descending' } = {},
+) {
+  return await Recipe.find(query).sort({ [sortBy]: sortOrder })
+}
+
+// List all the recipes =======================================================
+export async function listAllRecipes(options) {
+  return await listRecipes({}, options)
+}
+
+// List recipes by an author ==================================================
+export async function listRecipesByAuthor(author, options) {
+  return await listRecipes({ author }, options)
+}
