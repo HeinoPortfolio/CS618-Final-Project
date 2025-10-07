35d503270b5596160325757075e394e790bc5895
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-10-07 22:59:45 +0000

    chore:  implemented the ability to create a new post with dynamic update

--- src/components/CreateRecipe.jsx
+++ src/components/CreateRecipe.jsx
@@ -1,28 +1,102 @@
+import { useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createRecipe } from '../api/recipes.js'
+
 export function CreateRecipe() {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [ingredientList, setIngredientList] = useState('')
+  const [imageURL, setImageURL] = useState('')
+
+  // Create the query client ==================================================
+  const queryClient = useQueryClient()
+
+  // Create the Recipe mutation ===============================================
+  const createRecipeMutation = useMutation({
+    mutationFn: () => createRecipe({ title, author, ingredientList, imageURL }),
+    onSuccess: () => queryClient.invalidateQueries(['recipes']),
+  })
+
+  // External function to handle form submission ==============================
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createRecipeMutation.mutate()
+  }
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor='create-title'><b>  Recipe Title:</b> </label>
-        <input type='text' name='create-title' id='create-title' />
+        <label htmlFor='create-title'>
+          <b> Recipe Title:</b>{' '}
+        </label>
+        <input
+          type='text'
+          name='create-title'
+          id='create-title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <br />
       <div>
-        <label htmlFor='create-author'> <b> Author: </b></label>
-        <input type='text' name='create-author' id='create-author' />
+        <label htmlFor='create-author'>
+          {' '}
+          <b> Author: </b>
+        </label>
+        <input
+          type='text'
+          name='create-author'
+          id='create-author'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
       </div>
       <br />
       <div>
-        <label htmlFor='create-imageURL'> <b> URL for image: </b></label>
-        <input type='text' name='create-imageURL' id='create-imageURL' />
+        <label htmlFor='create-imageURL'>
+          {' '}
+          <b> URL for image: </b>
+        </label>
+        <input
+          type='text'
+          name='create-imageURL'
+          id='create-imageURL'
+          value={imageURL}
+          onChange={(e) => setImageURL(e.target.value)}
+        />
       </div>
-      <br/>
-      <label htmlFor='recibe-box'><b> Enter the recipe below: </b></label>
-      <br/>
       <br />
-      <textarea id='comment-box' name='recipe-area' rows='20' cols='60' />
+      <label htmlFor='recibe-box'>
+        <b> Enter the recipe below: </b>
+      </label>
       <br />
       <br />
-      <input type='submit' value='Create' />
+      <textarea
+        id='comment-box'
+        name='recipe-area'
+        rows='20'
+        cols='60'
+        value={ingredientList}
+        onChange={(e) => setIngredientList(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        type='submit'
+        value={
+          createRecipeMutation.isPending
+            ? 'Creating the recipe...'
+            : 'Create a new recipe'
+        }
+        disabled={!title}
+      />
+      {createRecipeMutation.isSuccess ? (
+        <>
+          <br />
+          <br/>
+          Recipe created successfully!
+        </>
+      ) : null}
     </form>
   )
 }
