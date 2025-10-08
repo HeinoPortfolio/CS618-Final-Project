aeffe83100b23c24072759c1a847a7498b3d3658
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-10-08 04:05:34 +0000

    chore: implemented routes for the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 // Express setup file =========================================================
 import express from 'express'
 import { recipesRoutes } from './routes/recipes.js'
+import { userRoutes } from './routes/users.js'
+
 import bodyParser from 'body-parser'
 
 import cors from 'cors'
@@ -10,6 +12,9 @@ app.use(bodyParser.json())
 app.use(cors())
 
 recipesRoutes(app)
+
+// User route ========================
+userRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express Live! Nodemon')
